8b9d0f8355a0cfc2eebffa2f506cedb39982c253
pmnoos <petermagner3@gmail.com>
2025-12-20 08:12:20 +1000

    Add all 32 chapters export

---
+++ chapters_data.rb
@@ -0,0 +1,192 @@
+Chapter.create!(
+  title: "A New Path: From Hospitality to the Post Office",
+  content: #<ActionText::RichText id: 10, name: "content", body: #<ActionText::Content "<div>A New Path: From ...">, record_type: "Chapter", record_id: 10, created_at: "2025-10-01 01:10:00.823979000 +0000", updated_at: "2025-10-01 01:10:00.823979000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Introduction",
+  content: #<ActionText::RichText id: 1, name: "content", body: #<ActionText::Content "<div>I’ve never been m...">, record_type: "Chapter", record_id: 1, created_at: "2025-09-28 02:42:39.137483000 +0000", updated_at: "2025-09-30 22:45:20.529796000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "A New Beginning: Herne Bay and Home ownership",
+  content: #<ActionText::RichText id: 11, name: "content", body: #<ActionText::Content "<div>The situation at ...">, record_type: "Chapter", record_id: 11, created_at: "2025-10-01 01:13:47.176559000 +0000", updated_at: "2025-10-01 01:13:47.176559000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Red Lion Cottages with Henry",
+  content: #<ActionText::RichText id: 8, name: "content", body: #<ActionText::Content "<div>&nbsp;Red Lion Co...">, record_type: "Chapter", record_id: 8, created_at: "2025-10-01 00:14:44.626119000 +0000", updated_at: "2025-10-01 01:03:48.429244000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Finding My Way: Leaving Clare House",
+  content: #<ActionText::RichText id: 7, name: "content", body: #<ActionText::Content "<div>After finishing m...">, record_type: "Chapter", record_id: 7, created_at: "2025-09-30 22:23:45.583050000 +0000", updated_at: "2025-12-11 10:57:59.388983000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Epping Forest",
+  content: #<ActionText::RichText id: 3, name: "content", body: #<ActionText::Content "<div>Our family had mo...">, record_type: "Chapter", record_id: 3, created_at: "2025-09-28 07:10:07.914507000 +0000", updated_at: "2025-09-30 22:51:34.319495000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Start of my childhood in Dover",
+  content: #<ActionText::RichText id: 4, name: "content", body: #<ActionText::Content "<div>found this <a hre...">, record_type: "Chapter", record_id: 4, created_at: "2025-09-28 07:22:21.478895000 +0000", updated_at: "2025-09-30 22:59:22.346087000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "After isolation",
+  content: #<ActionText::RichText id: 5, name: "content", body: #<ActionText::Content "<div><strong>After Iso...">, record_type: "Chapter", record_id: 5, created_at: "2025-09-30 03:25:39.733662000 +0000", updated_at: "2025-09-30 23:57:03.120546000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Claire House",
+  content: #<ActionText::RichText id: 6, name: "content", body: #<ActionText::Content "<div>Life’s twists and...">, record_type: "Chapter", record_id: 6, created_at: "2025-09-30 03:29:03.612495000 +0000", updated_at: "2025-09-30 23:59:42.487572000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "The Voyage Begins",
+  content: #<ActionText::RichText id: 16, name: "content", body: #<ActionText::Content "<div>The voyage became...">, record_type: "Chapter", record_id: 16, created_at: "2025-10-01 06:53:02.169056000 +0000", updated_at: "2025-12-16 10:45:20.698715000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Swan Hotel",
+  content: #<ActionText::RichText id: 9, name: "content", body: #<ActionText::Content "<div>Stratford-upon-Av...">, record_type: "Chapter", record_id: 9, created_at: "2025-10-01 00:18:25.582421000 +0000", updated_at: "2025-10-01 01:05:09.358191000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Shifting Ground: From Albion Place to Dungeness",
+  content: #<ActionText::RichText id: 12, name: "content", body: #<ActionText::Content "<div>After settling in...">, record_type: "Chapter", record_id: 12, created_at: "2025-10-01 03:38:56.729507000 +0000", updated_at: "2025-10-01 03:38:56.729507000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "A Kiwi Home, a Flying Career, and a New Chapter for Our Family",
+  content: #<ActionText::RichText id: 17, name: "content", body: #<ActionText::Content "<div>Arriving in New Z...">, record_type: "Chapter", record_id: 17, created_at: "2025-10-01 10:40:18.217081000 +0000", updated_at: "2025-10-01 10:40:18.217081000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Dreams Woven in Yarn and Sky: The Birth of Magner Knitwear",
+  content: #<ActionText::RichText id: 18, name: "content", body: #<ActionText::Content "<div>After the first y...">, record_type: "Chapter", record_id: 18, created_at: "2025-10-01 10:52:07.499379000 +0000", updated_at: "2025-10-01 10:52:07.499379000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Whitfield and the Ferries",
+  content: #<ActionText::RichText id: 13, name: "content", body: #<ActionText::Content "<div>Selling our littl...">, record_type: "Chapter", record_id: 13, created_at: "2025-10-01 03:44:27.464769000 +0000", updated_at: "2025-10-01 03:56:48.778310000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "The Strike and the Dream of New Zealand",
+  content: #<ActionText::RichText id: 14, name: "content", body: #<ActionText::Content "<div>By the mid-1960s,...">, record_type: "Chapter", record_id: 14, created_at: "2025-10-01 03:48:14.274103000 +0000", updated_at: "2025-10-01 03:58:05.862295000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Turning the Page: Family, Flight, and Fresh Beginnings",
+  content: #<ActionText::RichText id: 19, name: "content", body: #<ActionText::Content "<div>&nbsp;As we grew ...">, record_type: "Chapter", record_id: 19, created_at: "2025-10-02 02:03:22.978725000 +0000", updated_at: "2025-12-17 02:26:36.914900000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "A Journey Back to Where It All Began",
+  content: #<ActionText::RichText id: 21, name: "content", body: #<ActionText::Content "<div>Returning to the ...">, record_type: "Chapter", record_id: 21, created_at: "2025-10-02 07:24:50.768000000 +0000", updated_at: "2025-12-17 02:33:42.582903000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Looking after The Sussex Restaurant",
+  content: #<ActionText::RichText id: 22, name: "content", body: #<ActionText::Content "<div>In the mid-sevent...">, record_type: "Chapter", record_id: 22, created_at: "2025-10-03 03:22:33.154774000 +0000", updated_at: "2025-10-03 03:22:33.154774000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "The Beginning: February 18, 1940 — Sutton Coldfield, Birmingham, UK",
+  content: #<ActionText::RichText id: 2, name: "content", body: #<ActionText::Content "<div><strong>What time...">, record_type: "Chapter", record_id: 2, created_at: "2025-09-28 02:42:39.318005000 +0000", updated_at: "2025-09-30 22:49:23.434301000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "The Timeless Joy of New Zealand",
+  content: #<ActionText::RichText id: 20, name: "content", body: #<ActionText::Content "<div>I never really th...">, record_type: "Chapter", record_id: 20, created_at: "2025-10-02 02:11:42.505120000 +0000", updated_at: "2025-12-17 02:23:06.883218000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Epilogue",
+  content: #<ActionText::RichText id: 32, name: "content", body: #<ActionText::Content "<div>In a few weeks, I...">, record_type: "Chapter", record_id: 32, created_at: "2025-12-15 10:05:04.387796000 +0000", updated_at: "2025-12-17 10:44:05.569011000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "When Chaos Strikes Family ",
+  content: #<ActionText::RichText id: 31, name: "content", body: #<ActionText::Content "<div>Life has a way of...">, record_type: "Chapter", record_id: 31, created_at: "2025-12-15 02:40:01.908351000 +0000", updated_at: "2025-12-18 02:48:27.158055000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "When the Tide Turned",
+  content: #<ActionText::RichText id: 29, name: "content", body: #<ActionText::Content "<div>After selling Caf...">, record_type: "Chapter", record_id: 29, created_at: "2025-10-09 03:50:10.925207000 +0000", updated_at: "2025-12-14 23:55:05.187497000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "The Kitchen",
+  content: #<ActionText::RichText id: 24, name: "content", body: #<ActionText::Content "<div><strong>The Kitch...">, record_type: "Chapter", record_id: 24, created_at: "2025-10-06 21:44:10.671891000 +0000", updated_at: "2025-10-07 01:18:44.895697000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Setting Sail for a New Life",
+  content: #<ActionText::RichText id: 15, name: "content", body: #<ActionText::Content "<div>Receiving the let...">, record_type: "Chapter", record_id: 15, created_at: "2025-10-01 04:18:03.103143000 +0000", updated_at: "2025-10-01 04:18:03.103143000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "El Greco Pizza's ",
+  content: #<ActionText::RichText id: 25, name: "content", body: #<ActionText::Content "<h1><strong>El Greco –...">, record_type: "Chapter", record_id: 25, created_at: "2025-10-07 01:37:57.748938000 +0000", updated_at: "2025-10-07 01:37:57.748938000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "A New Beginning",
+  content: #<ActionText::RichText id: 30, name: "content", body: #<ActionText::Content "<div>Karen first came ...">, record_type: "Chapter", record_id: 30, created_at: "2025-10-10 06:46:21.527208000 +0000", updated_at: "2025-12-18 23:39:29.299534000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "From Britannia to Pizza – My Australian Culinary Adventure",
+  content: #<ActionText::RichText id: 26, name: "content", body: #<ActionText::Content "<div><strong>Landing i...">, record_type: "Chapter", record_id: 26, created_at: "2025-10-07 02:39:20.738413000 +0000", updated_at: "2025-10-07 04:13:38.063722000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Quicksand on Caxton Street",
+  content: #<ActionText::RichText id: 28, name: "content", body: #<ActionText::Content "<div>After the Bali Ha...">, record_type: "Chapter", record_id: 28, created_at: "2025-10-09 03:29:10.387960000 +0000", updated_at: "2025-10-09 03:34:57.588600000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Redvale and Beyond",
+  content: #<ActionText::RichText id: 23, name: "content", body: #<ActionText::Content "<div>Well, think again...">, record_type: "Chapter", record_id: 23, created_at: "2025-10-03 11:00:28.277057000 +0000", updated_at: "2025-12-17 02:36:04.245770000 +0000">,
+  user_id: 1
+)
+
+Chapter.create!(
+  title: "Bali Hai: Sunshine and Shadows",
+  content: #<ActionText::RichText id: 27, name: "content", body: #<ActionText::Content "<div>We owe it all to ...">, record_type: "Chapter", record_id: 27, created_at: "2025-10-08 22:26:11.313612000 +0000", updated_at: "2025-12-17 02:49:53.713827000 +0000">,
+  user_id: 1
+)
+
